a655f923c00da04c135c736012de4b580dd670ec
ashwayne <ashwin.deaglo@gmail.com>
2022-10-15 20:19:50 +0530

    not working evaporate multi_upload and working normal upload

--- video/urls.py
+++ video/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('upload', views.UploadView.as_view(template_name='create.html'), name='upload'),
+    path('evaupload', views.EvaUploadView.as_view(template_name='evaporate.html'), name='eva-upload'),
     path('map/<id>/', views.MapView.as_view(template_name='map.html'), name='map-page'),
     path('list', views.image_list, name='image_list'),
     path(r'api/files/policy/', views.FilePolicyAPI.as_view(), name='upload-policy'),
     path(r'api/upload/complete/', views.FileSuccessAPI.as_view(), name='upload-complete'),
     path(r'api/signedurl', views.FileAuthAPI.as_view(), name='signer-url'),
+    path(r'api/auth', views.EvaporateBaseExampleAPI.as_view(), name='auth-sign'),
 ]
--- video/views.py
+++ video/views.py
@@ -28,6 +28,17 @@ class UploadView(LoginRequiredMixin, CreateView):
         return context_data
 
 
+class EvaUploadView(LoginRequiredMixin, CreateView):
+    model = Upload
+    fields = ['name', 'image']
+
+    def get_context_data(self, **kwargs):
+        context_data = super(EvaUploadView, self).get_context_data(**kwargs)
+        context_data.update({"public_key": settings.AWS_ACCESS_KEY_ID, "bucket_name": settings.AWS_STORAGE_BUCKET_NAME,
+                             "signer_url": reverse('auth-sign')})
+        return context_data
+
+
 def image_list(request):
     images = Upload.objects.all()
     return render(request, 'list.html', {'images': images})
@@ -42,42 +53,28 @@ class FilePolicyAPI(APIView):
         Session Authentication but any auth should work.
         """
 
-        # filename_req = request.data.get('filename')
-        # if not filename_req:
-        #     return Response({"message": "A filename is required"}, status=status.HTTP_400_BAD_REQUEST)
-        # policy_expires = int(time.time() + 5000)
-        # """
-        # image_url - media/customer_slug/ortho/YYYYMMDD/uuid.<tif/png/jpg>
-        # """
-        # upload_start_path, obj_uuid, filename_final, username_str, ortho_obj = create_image_object(filename_req)
-        # url, policy, signature = get_policy(policy_expires, upload_start_path)
-        #
-        # data = {
-        #     "policy": policy,
-        #     "signature": signature,
-        #     "key": settings.AWS_ACCESS_KEY_ID,
-        #     "file_bucket_path": upload_start_path,
-        #     "file_uuid": obj_uuid,
-        #     "filename": filename_final,
-        #     "url": url,
-        #     "username": username_str,
-        # }
+        filename_req = request.data.get('filename')
+        if not filename_req:
+            return Response({"message": "A filename is required"}, status=status.HTTP_400_BAD_REQUEST)
+        policy_expires = int(time.time() + 5000)
+        """
+        image_url - media/customer_slug/ortho/YYYYMMDD/uuid.<tif/png/jpg>
+        """
+        upload_start_path, obj_uuid, filename_final, username_str, ortho_obj = create_image_object(filename_req)
+        url, policy, signature = get_policy(policy_expires, upload_start_path)
 
-        s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                                 aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY, region_name='us-west-2')
-        try:
-            response = s3_client.generate_presigned_post(settings.AWS_STORAGE_BUCKET_NAME, 'media/test_cat_obj_name.jpg',
-                                                         ExpiresIn=3600)
-            response['fields']["bucket"] = settings.AWS_STORAGE_BUCKET_NAME
-            response['fields']["file_uuid"] = "SAMPLE_UUID"
-            response['fields']["filename"] = 'test_cat_obj_name.jpg'
-            response['fields']["file_bucket_path"] = 'media/'
-            response['fields']["key"] = settings.AWS_ACCESS_KEY_ID
-        except Exception as e:
-            print("Exception while creating pre-signed url: ", e)
-            return Response({"error": e}, status=status.HTTP_400_BAD_REQUEST)
+        data = {
+            "policy": policy,
+            "signature": signature,
+            "key": settings.AWS_ACCESS_KEY_ID,
+            "file_bucket_path": upload_start_path,
+            "file_uuid": obj_uuid,
+            "filename": filename_final,
+            "url": url,
+            "username": username_str,
+        }
 
-        return Response(response, status=status.HTTP_200_OK)
+        return Response(data, status=status.HTTP_200_OK)
 
 
 class MapView(LoginRequiredMixin, DetailView):
@@ -162,3 +159,63 @@ class FileAuthAPI(APIView):
         print("*" * 50)
 
         return Response(signature, status=status.HTTP_200_OK, content_type="text/HTML")
+
+
+class EvaporateBaseExampleAPI(APIView):
+
+    def get(self, request, *args, **kwargs):
+
+        to_sign = str(request.GET.get('to_sign')).encode('utf-8')
+
+        aws_secret = 'Dhqy88bO/9FrR0TeYDEwa5uRyA69ExHm68tok4YE'
+        date_stamp = datetime.datetime.strptime(request.GET.get('datetime'), '%Y%m%dT%H%M%SZ').strftime('%Y%m%d')
+        region = 'us-west-2'
+        service = 's3'
+
+        # Key derivation functions. See:
+        # http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
+        def sign(key, msg):
+            return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+
+        def getSignatureKey(key, date_stamp, regionName, serviceName):
+            kDate = sign(('AWS4' + key).encode('utf-8'), date_stamp)
+            kRegion = sign(kDate, regionName)
+            kService = sign(kRegion, serviceName)
+            kSigning = sign(kService, 'aws4_request')
+            return kSigning
+
+        signing_key = getSignatureKey(aws_secret, date_stamp, region, service)
+
+        # Sign to_sign using the signing_key
+        signature = hmac.new(
+            signing_key,
+            to_sign,
+            hashlib.sha256
+        ).hexdigest()
+
+        return Response(signature, status=status.HTTP_200_OK, content_type="text/HTML")
+
+
+# class EvaporatePolicyAPI(APIView):
+#
+#     def post(self, request, *args, **kwargs):
+#         """
+#         Evaporate API separate view
+#         """
+#
+#         s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+#                                  aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY, region_name='us-west-2')
+#         try:
+#             response = s3_client.generate_presigned_post(settings.AWS_STORAGE_BUCKET_NAME,
+#                                                          'media/test_cat_obj_name.jpg',
+#                                                          ExpiresIn=3600)
+#             response['fields']["bucket"] = settings.AWS_STORAGE_BUCKET_NAME
+#             response['fields']["file_uuid"] = "SAMPLE_UUID"
+#             response['fields']["filename"] = 'test_cat_obj_name.jpg'
+#             response['fields']["file_bucket_path"] = 'media/'
+#             response['fields']["key"] = settings.AWS_ACCESS_KEY_ID
+#         except Exception as e:
+#             print("Exception while creating pre-signed url: ", e)
+#             return Response({"error": e}, status=status.HTTP_400_BAD_REQUEST)
+#
+#         return Response(response, status=status.HTTP_200_OK)
